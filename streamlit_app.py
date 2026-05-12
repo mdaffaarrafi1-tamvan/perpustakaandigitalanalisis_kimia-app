63c38b32f9dbf25efbc8a0365d16f81bbcc93b2a
mdaffaarrafi1-tamvan <mdaffaarrafi1@gmail.com>
2026-05-12 09:06:51 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,18 +12,3 @@ else:
 
 if st.button("Aloha", type="tertiary"):
     st.write("Ciao")
-st.write("1 + 1 = ", 2)
-import altair as alt
-import pandas as pd
-import streamlit as st
-from numpy.random import default_rng as rng
-
-df = pd.DataFrame(rng(0).standard_normal((200, 3)), columns=["a", "b", "c"])
-chart = (
-    alt.Chart(df)
-    .mark_circle()
-    .encode(x="a", y="b", size="c", color="c", tooltip=["a", "b", "c"])
-)
-
-st.write(chart)
-st.image("sunrise.jpg", caption="Sunrise by the mountains")
