f7950bfe7ab545ca2922231c125a99bec3b60657
mdaffaarrafi1-tamvan <mdaffaarrafi1@gmail.com>
2026-05-12 08:46:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,16 @@ st.write(
 )
 
 st.write("1 + 1 = ", 2)
-st.write("Below is a DataFrame:", data_frame, "Above is a dataframe.")
+import altair as alt
+import pandas as pd
+import streamlit as st
+from numpy.random import default_rng as rng
+
+df = pd.DataFrame(rng(0).standard_normal((200, 3)), columns=["a", "b", "c"])
+chart = (
+    alt.Chart(df)
+    .mark_circle()
+    .encode(x="a", y="b", size="c", color="c", tooltip=["a", "b", "c"])
+)
+
+st.write(chart)
