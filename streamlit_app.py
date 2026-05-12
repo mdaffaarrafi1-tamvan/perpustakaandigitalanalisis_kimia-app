efa12e7e7651b42f1bf76eb30f7054dc4eb114df
mdaffaarrafi1-tamvan <mdaffaarrafi1@gmail.com>
2026-05-12 09:08:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ st.header("Aplikasi Operasi Aritmatika, divider=1True)
 number1 = st.number_input("masukkan angka 1")          
 number2 = st.number_input("masukkan angka 2")      
 st.button("Reset", type="primary")
+
 if st.button("Say hello"):
     st.write("Why hello there")
 else:
