17eb28932effe0f76175ed83289fc466c1a294bf
mdaffaarrafi1-tamvan <mdaffaarrafi1@gmail.com>
2026-05-12 08:45:08 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,6 @@ st.title("🎈 WELCOME TO DAPPA CUKURUKUK, AND MY WORD IS NGAWIFASHION")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-import streamlit as st
 
 st.write("1 + 1 = ", 2)
 st.write("Below is a DataFrame:", data_frame, "Above is a dataframe.")
