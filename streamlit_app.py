0f28262be703111704c1ec57aec84324ff72e2e8
mdaffaarrafi1-tamvan <mdaffaarrafi1@gmail.com>
2026-05-12 08:48:58 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 WELCOME TO DAPPA CUKURUKUK, AND MY WORD IS NGAWIFASHION")
+st.title("🎈Ikan Goreng  ")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
