c7c9056e50f92d790384e2bf76d646f4df08ca7d
mdaffaarrafi1-tamvan <mdaffaarrafi1@gmail.com>
2026-05-12 09:06:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,17 @@
 import streamlit as st
 
 st.title("🎈Ikan Goreng  ")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.header("Aplikasi Operasi Aritmatika, divider=1True)
+number1 = st.number_input("masukkan angka 1")          
+number2 = st.number_input("masukkan angka 2")      
+st.button("Reset", type="primary")
+if st.button("Say hello"):
+    st.write("Why hello there")
+else:
+    st.write("Goodbye")
 
+if st.button("Aloha", type="tertiary"):
+    st.write("Ciao")
 st.write("1 + 1 = ", 2)
 import altair as alt
 import pandas as pd
