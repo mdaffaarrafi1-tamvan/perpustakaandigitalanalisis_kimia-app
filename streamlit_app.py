e9f8b50a52dcd9021feed734e5a05324017a58eb
mdaffaarrafi1-tamvan <mdaffaarrafi1@gmail.com>
2026-05-23 09:40:56 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,6 @@
 import streamlit as st
 
-st.title("🎈Ikan Goreng  ")
-st.header("Aplikasi Operasi Aritmatika")
-number1 = st.number_input("masukkan angka 1")          
-number2 = st.number_input("masukkan angka 2")      
-st.button("Reset", type="primary")
-
-if st.button("Say hello"):
-    st.write("Why hello there")
-else:
-    st.write("Goodbye")
-
-if st.button("Aloha", type="tertiary"):
-    st.write("Ciao")
+st.set_page_config(
+  page_title="Finance Dashboard",
+  layout="wide"
+)
